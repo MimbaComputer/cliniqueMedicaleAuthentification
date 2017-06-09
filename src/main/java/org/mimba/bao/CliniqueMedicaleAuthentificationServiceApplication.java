e6382561b102ec052a51543615604ae06c56cab5
Mimba <baounabaouna@gmail.com>
2017-06-09 14:12:31 +0100

    Les couches DAI et Entities achevées.

--- src/main/java/org/mimba/bao/CliniqueMedicaleAuthentificationServiceApplication.java
+++ src/main/java/org/mimba/bao/CliniqueMedicaleAuthentificationServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication(exclude = EndpointMBeanExportAutoConfiguration.class)
 public class CliniqueMedicaleAuthentificationServiceApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(CliniqueMedicaleAuthentificationServiceApplication.class, args);
 	}
 
